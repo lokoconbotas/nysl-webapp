43f7c350745c5590c7f1a4b0968cdf4c2eca9024
Raul <lokoconbotas@gmail.com>
2016-11-20 01:23:05 +0100

    Añadido Scroll
    Ahora todo el HTML está en un único archivo...

--- public/media/js/open-links.js
+++ public/media/js/open-links.js
@@ -1,16 +1,42 @@
-var a=document.getElementsByTagName("a");
-            for(var i=0;i<a.length;i++)
-            {
-                a[i].onclick=function()
-                {
-                    window.location=this.getAttribute("href");
-                    return false
-                }
-            }
-
 $(document).ready(function(){
     $('.row-info3').on('click', function(){
         var object = $(this).parent().parent().parent();
-        $('html,body').animate({scrollTop: $(object).offset().top}, 800);
+        $('html,body').animate({scrollTop: $(object).offset().top}, 300);
     }); 
 });
+
+$('#boton1').on('click', function() {
+    $('#page1').show();
+    $('#page2').hide();
+    
+    $('#icon1').addClass("active");
+    $("#icon2").removeClass("active");
+    $("#icon3").removeClass("active");
+    $("#icon4").removeClass("active");
+
+    //$('body').animate({scrollTop: $('#page1').offset().top}, 600);
+
+});
+
+
+$('#boton2').on('click', function() {
+    $('#page1').hide();
+    $('#page2').show();
+    
+    $('#icon1').removeClass("active");
+    $("#icon2").removeClass("active");
+    $("#icon3").addClass("active");
+    $("#icon4").removeClass("active");
+
+    //$('body').animate({scrollTop: $('#page2').offset().top}, 600);
+
+});
+
+
+
+//$(document).ready(function(){
+//    $('.page1').on('click', function(){
+//        $('html,body').animate({scrollTop: $('#page1').offset().top}, 800);
+//        console.log("smooth2");
+//    }); 
+//});
