408652290f509b31920579d4e44d02cbf0ab2296
Raul <lokoconbotas@gmail.com>
2016-11-21 16:40:22 +0100

    Implementado 100% fullpage.js
    Funcionan todas las páginas con el scroll. Cambiado el estilo

--- public/media/js/open-links.js
+++ public/media/js/open-links.js
@@ -1,37 +1,71 @@
+window.onorientationchange = function() { 
+        var orientation = window.orientation; 
+            switch(orientation) { 
+                case 0:
+                case 90:
+                case -90: window.location.reload(); 
+                break; } 
+    };
+
+$('#page1').show();
+    $('#page2').hide();
+
+
+
 $(document).ready(function(){
     $('.row-info3').on('click', function(){
         var object = $(this).parent().parent().parent();
-        $('.intro').animate({scrollTop: $(object).offset().top}, 300);
-        console.log(object);
+        $('html, body').animate({scrollTop: $(object).offset().top}, 300);
+        //console.log(object);
     }); 
 });
 
-//$('#boton1').on('click', function() {
+$('#boton1').on('click', function() {
 //    $('#page1').show();
 //    $('#page2').hide();
-//    
-//    $('#icon1').addClass("active");
-//    $("#icon2").removeClass("active");
-//    $("#icon3").removeClass("active");
-//    $("#icon4").removeClass("active");
-//
-//    //$('body').animate({scrollTop: $('#page1').offset().top}, 600);
-//
-//});
-//
-//
-//$('#boton2').on('click', function() {
+//    $('#page3').hide();
+//    //$('#page4').hide();
+    
+    $('#icon1').addClass("active");
+    $("#icon2").removeClass("active");
+    $("#icon3").removeClass("active");
+    $("#icon4").removeClass("active");
+
+    //$('body').animate({scrollTop: $('#page1').offset().top}, 600);
+
+});
+
+
+$('#boton2').on('click', function() {
 //    $('#page1').hide();
 //    $('#page2').show();
-//    
-//    $('#icon1').removeClass("active");
-//    $("#icon2").removeClass("active");
-//    $("#icon3").addClass("active");
-//    $("#icon4").removeClass("active");
-//
-//    //$('body').animate({scrollTop: $('#page2').offset().top}, 600);
-//
-//});
+//    $('#page3').hide();
+//    //$('#page4').hide();
+    
+    $('#icon1').removeClass("active");
+    $("#icon2").removeClass("active");
+    $("#icon3").addClass("active");
+    $("#icon4").removeClass("active");
+
+    //$('body').animate({scrollTop: $('#page2').offset().top}, 600);
+
+});
+
+
+$('#boton3').on('click', function() {
+//    $('#page1').hide();
+//    $('#page2').hide();
+//    $('#page3').show();
+//    //$('#page4').hide();
+    
+    $('#icon1').removeClass("active");
+    $("#icon2").removeClass("active");
+    $("#icon3").addClass("active");
+    $("#icon4").removeClass("active");
+
+    //$('body').animate({scrollTop: $('#page1').offset().top}, 600);
+
+});
 
 
 //$(document).ready(function(){
