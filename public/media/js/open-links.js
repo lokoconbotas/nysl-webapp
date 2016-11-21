1e730a2d17ced45e5c165fb57b974a0b5ada6331
Raul <lokoconbotas@gmail.com>
2016-11-21 01:22:18 +0100

    Added Single App. Functional
    Se añade la librería jqfullpage funcionando en casi su totalidad

--- public/media/js/open-links.js
+++ public/media/js/open-links.js
@@ -1,36 +1,37 @@
 $(document).ready(function(){
     $('.row-info3').on('click', function(){
         var object = $(this).parent().parent().parent();
-        $('html,body').animate({scrollTop: $(object).offset().top}, 300);
+        $('.intro').animate({scrollTop: $(object).offset().top}, 300);
+        console.log(object);
     }); 
 });
 
-$('#boton1').on('click', function() {
-    $('#page1').show();
-    $('#page2').hide();
-    
-    $('#icon1').addClass("active");
-    $("#icon2").removeClass("active");
-    $("#icon3").removeClass("active");
-    $("#icon4").removeClass("active");
-
-    //$('body').animate({scrollTop: $('#page1').offset().top}, 600);
-
-});
-
-
-$('#boton2').on('click', function() {
-    $('#page1').hide();
-    $('#page2').show();
-    
-    $('#icon1').removeClass("active");
-    $("#icon2").removeClass("active");
-    $("#icon3").addClass("active");
-    $("#icon4").removeClass("active");
-
-    //$('body').animate({scrollTop: $('#page2').offset().top}, 600);
-
-});
+//$('#boton1').on('click', function() {
+//    $('#page1').show();
+//    $('#page2').hide();
+//    
+//    $('#icon1').addClass("active");
+//    $("#icon2").removeClass("active");
+//    $("#icon3").removeClass("active");
+//    $("#icon4").removeClass("active");
+//
+//    //$('body').animate({scrollTop: $('#page1').offset().top}, 600);
+//
+//});
+//
+//
+//$('#boton2').on('click', function() {
+//    $('#page1').hide();
+//    $('#page2').show();
+//    
+//    $('#icon1').removeClass("active");
+//    $("#icon2").removeClass("active");
+//    $("#icon3").addClass("active");
+//    $("#icon4").removeClass("active");
+//
+//    //$('body').animate({scrollTop: $('#page2').offset().top}, 600);
+//
+//});
 
 
 //$(document).ready(function(){
