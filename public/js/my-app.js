ef01012174e7ee81f40fd72078316319145ded72
Raul <lokoconbotas@gmail.com>
2016-11-24 18:33:06 +0100

    Estilo pulido y añadido chat Firebase

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -13,3 +13,16 @@ var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
+
+var myPhotoBrowserStandalone = myApp.photoBrowser({
+    photos : [
+        'http://lorempixel.com/1024/1024/sports/1/',
+        'http://lorempixel.com/1024/1024/sports/2/',
+        'http://lorempixel.com/1024/1024/sports/3/',
+    ]
+});
+//Open photo browser on click
+$$('.pb-standalone').on('click', function () {
+    console.log("hola");
+    myPhotoBrowserStandalone.open();
+});
