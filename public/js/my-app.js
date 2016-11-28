4b984b8dfce017958f1cae4880db431b48db977e
Raul <lokoconbotas@gmail.com>
2016-11-28 17:04:57 +0100

    Creating Tables dinamically
    Empezando desde la de Matches, queda implementar Angular para ir creando las páginas de  Game Info de manera automática al clickar cada partido

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -13,3 +13,106 @@ var mainView = myApp.addView('.view-main', {
     // Enable Dom Cache so we can use all inline pages
     domCache: true
 });
+
+var nyslApp = angular.module('nyslApp', []);
+
+nyslApp.controller('nyslController', function ($scope, $http){
+
+    $scope.fillGameInfoData = function() {
+
+        $http({
+            method: 'GET',
+            url: "js/nysl-sheet.json"
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            //console.log(response);
+            var datos = response.data.Matches;
+            $scope.datos = datos;
+
+            $scope.message = datos[0].teams.field;
+
+
+
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.error(response);
+        });
+
+       
+
+
+    }
+
+
+});
+
+
+$.getJSON("js/nysl-sheet.json", function(result){
+
+    var datos = result.Matches;
+
+
+
+
+    dibujarTablaPartidos(datos)
+
+
+});
+
+
+
+
+
+
+function dibujarTablaPartidos(datos){
+
+
+
+    datos.forEach(function(team){
+
+        $("#partidos-a-crear")
+            .append($("<li>")
+                    .append($('<a>')
+                            .attr("href", "#gameInfo")
+                            .attr("data-view", ".view-main")
+                            .addClass("item-link")
+                            .append($("<div>")
+                                    .addClass("item-content")
+                                    .addClass("item-merged")
+                                    .append($("<div>")
+                                            .addClass("item-inner")
+                                            .append($("<div>")
+                                                    .addClass("item-title")
+                                                    .append($("<div>")
+                                                            .addClass("teamName")
+                                                            .append("<img class='escudos' src='"+ team.teams.t1logo +"'/>")
+                                                            .append($("<span>")
+                                                                    .append(team.teams.t1name)))
+                                                    .append($("<div>")
+                                                            .addClass("teamName")
+                                                            .append("<img class='escudos' src='"+ team.teams.t2logo +"'/>")
+                                                            .append($("<span>")
+                                                                    .append(team.teams.t2name))))))));
+
+    });
+
+
+
+};
+
+
+$(".pre-splash").hide();
+
+$("#show-log").click(function() {
+
+    $(".pre-splash").toggle();
+
+});
+
+$("#show-hide").click(function(){
+
+    $("#info-entrada").hide();
+
+});
